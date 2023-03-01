f302bfe4d046e4ec3359fe470951fddce63184ec
Artemelka <aemelianov85@gmail.com>
2023-03-01 20:42:25 +0200

    fix const

--- client/src/pages/games-page/games-page.tsx
+++ client/src/pages/games-page/games-page.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { RoutesPage } from '@/components';
-import { PAGES_PATH } from "@/pages/constants";
+import { GAMES_PAGE_PATH } from './constants';
 import { GAMES_CHILDREN_PAGE_ROUTE_CONFIG } from './children';
 
 const GamesPage: FC = () => (
   <RoutesPage
-    targetPath={PAGES_PATH.GAMES}
+    targetPath={GAMES_PAGE_PATH}
     routesConfig={GAMES_CHILDREN_PAGE_ROUTE_CONFIG}
   />
 );
--- client/src/pages/implementation-page/implementation-page.tsx
+++ client/src/pages/implementation-page/implementation-page.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { RoutesPage } from '@/components';
-import { PAGES_PATH } from '../constants';
+import { IMPLEMENTATION_PAGE_PATH } from './constants';
 import { IMPLEMENTATION_CHILDREN_PAGE_ROUTE_CONFIG } from './children';
 
 const ImplementationPage: FC = () => (
   <RoutesPage
-    targetPath={PAGES_PATH.IMPLEMENTATION}
+    targetPath={IMPLEMENTATION_PAGE_PATH}
     routesConfig={IMPLEMENTATION_CHILDREN_PAGE_ROUTE_CONFIG}
   />
 );
